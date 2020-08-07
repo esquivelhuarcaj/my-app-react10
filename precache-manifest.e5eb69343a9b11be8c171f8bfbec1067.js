6afdcde66f7c35ec53e6055ba6f93e37cf86d3a0
esquivelhuarcaj <esquivelhuarcaj@gmail.com>
2020-08-06 20:58:03 -0500

    Updates

---
+++ precache-manifest.e5eb69343a9b11be8c171f8bfbec1067.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826f2d29d0b3a46475c2733fcbfb1dad",
+    "url": "/my-app-react10/index.html"
+  },
+  {
+    "revision": "8aa23bc081e95c5ac5ba",
+    "url": "/my-app-react10/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/my-app-react10/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-react10/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa23bc081e95c5ac5ba",
+    "url": "/my-app-react10/static/js/main.324d774d.chunk.js"
+  },
+  {
+    "revision": "93406bd2561d29cd867f",
+    "url": "/my-app-react10/static/js/runtime-main.08c4a124.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-react10/static/media/logo.5d5d9eef.svg"
+  }
+]);
